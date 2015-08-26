6bb86bc51f2b9a229ca4cf82a65a1a72683cbb7b
anambiar <amith.nmbr@gmail.com>
2015-08-26 23:04:05 +1000

    Printing colum names

--- src/main/scala/com/voicestreams/sql/ReadAvro.scala
+++ src/main/scala/com/voicestreams/sql/ReadAvro.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object ReadAvro extends App {
+
   val sparkConf = new SparkConf().setAppName("Avro Reader Job")
     .setMaster("local[4]")
 
@@ -12,6 +13,11 @@ object ReadAvro extends App {
   val df = sqlContext.load("src/main/resources/episodes.avro", "com.databricks.spark.avro")
 
   df.registerTempTable("lines")
+
+  for(column <- df.columns) {
+    println(column)
+  }
+
   sqlContext.sql("select title, doctor from lines where doctor > 5 order by doctor asc limit 5").show(5)
 }
 
